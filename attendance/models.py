7b7b3ac78e3b066040c4d043626e5086a568760a
Sri-ram100 <119070794+Sri-ram100@users.noreply.github.com>
2024-11-08 12:43:42 +0530

    final

--- attendance/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class AttendanceRecord(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
-
-    class Meta:
-        unique_together = ('user', 'date')
